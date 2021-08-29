dab3306ea07243ea5110fe57f6824f8bcf47c8ed
katiebbugs <79028196+katiebbugs@users.noreply.github.com>
2021-08-29 17:56:43 -0600

    deploying to heroku

---
+++ client/build/precache-manifest.6923fc5a3e01639a1be727979d21b042.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8c9e778ddd8a9c6aacbb5b4a0995f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2380ac8e50393413e4f",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "8242555c23c7edec5b6a",
+    "url": "/static/css/main.0ad36afd.chunk.css"
+  },
+  {
+    "revision": "a2380ac8e50393413e4f",
+    "url": "/static/js/2.305689bc.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.305689bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8242555c23c7edec5b6a",
+    "url": "/static/js/main.c0d8a1dd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "694e591f81360f07e9e13e580c2ecd7b",
+    "url": "/static/media/beanEater.694e591f.gif"
+  }
+]);
