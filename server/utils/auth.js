fdba16639aca7dcd9ca73d655fba89f5ec70133d
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-28 21:50:52 -0600

    removed node_modules from root

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const secret = process.env.SECRET
 const expiration = '2h'
+
 
 module.exports = {
   authMiddleware: function ({ req }) {
